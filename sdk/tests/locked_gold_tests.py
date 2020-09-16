bec5ed5b1a9b83de3d4ba6a1d2dae8a52d9919e8
vadimBlaize <vadim.g@blaize.tech>
2020-09-16 09:58:38 +0300

    Add blank for LockedGold tests

---
+++ sdk/tests/locked_gold_tests.py
@@ -0,0 +1,20 @@
+import time
+import unittest
+
+from web3 import Web3
+
+from sdk.kit import Kit
+from sdk.tests import test_data
+
+
+class TestLockedGoldWrapper(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.kit = Kit('https://alfajores-forno.celo-testnet.org')
+        self.locked_gold_wrapper = self.kit.base_wrapper.create_and_get_contract_by_name(
+            'LockedGold')
+        self.kit.wallet_add_new_key = test_data.pk1
+
+    def test_name(self):
+        print(self.locked_gold_wrapper.lock())
